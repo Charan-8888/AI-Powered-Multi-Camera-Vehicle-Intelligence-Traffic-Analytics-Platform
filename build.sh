6f0ebcc1098db6a844cde284bb93fa9e602d9424
Charan-8888 <arabaticharan12908@gmail.com>
2026-09-21 23:33:22 +0530

    chore: add seed_demo_data to Render build step

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ set -o errexit
 pip install -r backend/requirements.txt
 python backend/manage.py collectstatic --noinput
 python backend/manage.py migrate --noinput
+python backend/manage.py seed_demo_data
